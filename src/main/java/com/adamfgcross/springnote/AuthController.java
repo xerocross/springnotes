3654c8c268ce29309ebcbbe7c2de2d61447cd1e6
Adam <adamfgcross@gmail.com>
2024-08-20 08:46:54 -0400

    add logging to authcontroller

--- src/main/java/com/adamfgcross/springnote/AuthController.java
+++ src/main/java/com/adamfgcross/springnote/AuthController.java
@@ -4,12 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.adamfgcross.springnote.auth.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -21,6 +26,7 @@ public class AuthController {
             String token = authService.authenticate(authRequest.getUsername(), authRequest.getPassword());
             return ResponseEntity.ok(new AuthResponse(token));
         } catch (Exception e) {
+        	logger.error(e.toString());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
         }
     }
