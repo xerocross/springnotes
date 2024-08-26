8c426cb2a76c2939d16538e135aedf1abeb2aca9
Adam <adamfgcross@gmail.com>
2024-08-26 11:24:33 -0400

    add logic to note service for creating keywords when creating new note

--- src/main/java/com/adamfgcross/springnote/NoteService.java
+++ src/main/java/com/adamfgcross/springnote/NoteService.java
@@ -1,15 +1,17 @@
 package com.adamfgcross.springnote;
 
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.adamfgcross.springnote.data.KeywordRepository;
 import com.adamfgcross.springnote.data.NoteRepository;
 import com.adamfgcross.springnote.entities.Keyword;
 import com.adamfgcross.springnote.entities.Note;
 import com.adamfgcross.springnote.entities.User;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -18,13 +20,30 @@ import java.util.regex.Pattern;
 @Service
 public class NoteService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(NoteService.class);
+	
 	@Autowired
 	private NoteRepository noteRepository;
+	
+	@Autowired
+	private KeywordRepository keywordRepository;
 	
 	public Note createNote(User user, NoteInput noteInput) {
 		Note note = new Note();
 		note.setUser(user);
 		note.setText(noteInput.getText());
+		noteRepository.save(note);
+		String text = noteInput.getText();
+		logger.info("note text: " + text);
+		Set<Keyword> keywords = parseKeywords(text);
+		
+		keywords.forEach(keyword -> {
+			logger.info("found keyword: " + keyword.getKeyword());
+			note.getKeywords().add(keyword);
+			keyword.getNotes().add(note);
+			keywordRepository.save(keyword);
+		});
+
 		return noteRepository.save(note);
 	}
 	
